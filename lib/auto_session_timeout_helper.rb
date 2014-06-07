82bc77698e9df1a7488f64134e159681d2bbdb38
Nicholai Hsu <nicholai@agilecredit.com>
2014-06-07 11:11:05 +0800

    Assign updater to variable for later stop on signout

--- lib/auto_session_timeout_helper.rb
+++ lib/auto_session_timeout_helper.rb
@@ -3,11 +3,11 @@ module AutoSessionTimeoutHelper
     frequency = options[:frequency] || 60
     code = <<JS
 if (typeof(Ajax) != 'undefined') {
-  new Ajax.PeriodicalUpdater('', '/active', {frequency:#{frequency}, method:'get', onSuccess: function(e) {
+  timeout = new Ajax.PeriodicalUpdater('', '/active', {frequency:#{frequency}, method:'get', onSuccess: function(e) {
     if (e.responseText == 'false') window.location.href = '/timeout';
   }});
 } else {
-  $.PeriodicalUpdater('/active', {minTimeout:#{frequency * 1000}, multiplier:0, method:'get', verbose:2}, function(remoteData, success) {
+  timeout = $.PeriodicalUpdater('/active', {minTimeout:#{frequency * 1000}, multiplier:0, method:'get', verbose:2}, function(remoteData, success) {
     if (success == 'success' && remoteData == 'false')
       window.location.href = '/timeout';
   });
